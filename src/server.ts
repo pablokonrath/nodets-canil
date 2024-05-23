b8d8265110f5d825895d87a99f67bca74eb21b1e
pablokonrath <pabloak@live.com>
2024-05-23 13:35:17 -0300

    final project 1.0

--- src/server.ts
+++ src/server.ts
@@ -18,7 +18,7 @@ server.use(Express.static(path.join(__dirname, '../public')));
 server.use(mainRoutes);
 //
 server.use((req, res)=>{
-  res.send('pagina não encontrada');
+  res.render('pages/404');
 });
 
 server.listen(process.env.PORT);
